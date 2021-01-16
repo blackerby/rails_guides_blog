e449b362296a073e6184172e70b76c2c1d5d05ec
blackerby <wmblackerby@gmail.com>
2021-01-15 21:37:09 -0600

    Add basic auth

--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,4 +1,6 @@
 class ArticlesController < ApplicationController
+  http_basic_authenticate_with name: "wtb", password: "$ecret!", except: [:index, :show]
+
   def index
     @articles = Article.all
   end
